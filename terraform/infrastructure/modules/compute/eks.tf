156458cdbac8b34ddf755276ed079528bb006253
Andres Solarte <devsolarte@gmail.com>
2025-11-19 09:50:39 -0500

    Add EKS blueprints addons module with AWS EBS CSI driver, CoreDNS, VPC CNI, and kube-proxy configurations

--- terraform/infrastructure/modules/compute/eks.tf
+++ terraform/infrastructure/modules/compute/eks.tf
@@ -35,3 +35,33 @@ module "eks" {
     }
   }
 }
+
+
+module "eks_blueprints_addons" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "~> 1.22.0"
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  eks_addons = {
+    aws-ebs-csi-driver = {
+      most_recent = true
+    }
+    coredns = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+  }
+
+  enable_aws_load_balancer_controller = true
+  enable_metrics_server               = true
+  enable_external_dns                 = true
+}
