e1326e7d65d8abcb26eb8b6af738633181d82006
Andres Solarte <devsolarte@gmail.com>
2025-11-20 07:28:28 -0500

    stack deleted

--- terraform/infrastructure/environments/main/backend.tf
+++ terraform/infrastructure/environments/main/backend.tf
@@ -1,8 +1,12 @@
 terraform {
-  backend "s3" {
-    bucket  = "nodesource-main-nsolid-tt-app-tf-state"
-    key     = "nodesource/main/nsolid-tt-app/terraform.tfstate"
-    region  = "us-east-2"
-    encrypt = true
-  }
+  # backend "s3" {
+  #   bucket  = "nodesource-main-nsolid-tt-app-tf-state"
+  #   key     = "nodesource/main/nsolid-tt-app/terraform.tfstate"
+  #   region  = "us-east-2"
+  #   encrypt = true
+  # }
+
+  # backend "local" {
+  #   path = "terraform.tfstate"
+  # }
 }
